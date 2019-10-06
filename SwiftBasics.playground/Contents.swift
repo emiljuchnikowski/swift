b4e524a25588a53d44c52f231bc90dc8fa5d4166
Emil Juchnikowski <emil@juchnikowski.com>
2019-10-06 22:17:09 +0200

    Maths doubles and Floats

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -12,3 +12,15 @@ let name : String = "Emil"
 
 // wyswietlanie w konsoli
 print(name)
+
+
+// Maths doubles and Floats
+print(7 / 4) // 1
+print(7.00 / 4.00) // 1.75
+
+var weight = 83.45633 // Double, default type
+var weight2 : Double = 83.45633 // Double
+var weight3 = 83.45633 // Float
+
+age * Int(weight) // Parse to Int
+Double(age) * weight // Parse to Double
