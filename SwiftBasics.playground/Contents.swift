43424c39234c56112a1c0efdb8e920a9d39cbad2
Emil Juchnikowski <emil@juchnikowski.com>
2019-10-06 22:29:30 +0200

    Booleans and if stamtements

--- SwiftBasics.playground/Contents.swift
+++ SwiftBasics.playground/Contents.swift
@@ -1,26 +1,39 @@
 import Foundation
+//
+//var age : Int = 30
+//
+//let name : String = "Emil"
+//
+//// Komentarz
+//
+///*
+// Komentarz 2
+// */
+//
+//// wyswietlanie w konsoli
+//print(name)
+//
+//
+//// Maths doubles and Floats
+//print(7 / 4) // 1
+//print(7.00 / 4.00) // 1.75
+//
+//var weight = 83.45633 // Double, default type
+//var weight2 : Double = 83.45633 // Double
+//var weight3 = 83.45633 // Float
+//
+//age * Int(weight) // Parse to Int
+//Double(age) * weight // Parse to Double
 
-var age : Int = 30
 
-let name : String = "Emil"
+// Booleans and if stamtements
+var height = 176
 
-// Komentarz
+height > 60
 
-/*
- Komentarz 2
- */
+if height > 10 {
+    print("Test")
+}
 
-// wyswietlanie w konsoli
-print(name)
+var can: Bool = true
 
-
-// Maths doubles and Floats
-print(7 / 4) // 1
-print(7.00 / 4.00) // 1.75
-
-var weight = 83.45633 // Double, default type
-var weight2 : Double = 83.45633 // Double
-var weight3 = 83.45633 // Float
-
-age * Int(weight) // Parse to Int
-Double(age) * weight // Parse to Double
