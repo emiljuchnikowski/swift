35af0b4a758901ad48b2748b7b5d9700b99c0e88
Emil Juchnikowski <emil@juchnikowski.com>
2019-10-10 11:06:21 +0200

    Dictionaries

--- SwiftIntermediate.playground/Contents.swift
+++ SwiftIntermediate.playground/Contents.swift
@@ -10,3 +10,11 @@ luckyNumbers.contains(2) // sprawdzanie obecności
 luckyNumbers.insert(5) // dodawanie
 luckyNumbers.insert(1)
 luckyNumbers
+
+// Dictionaries
+
+var nicksWords : [String:String] = ["zabang" : "when you fianlly get your code right", "hooha": "When you've..."]
+
+nicksWords["zabang"] // pobieranie wartości
+nicksWords["test1"] = "testVal" // dodawanie i aktualizacja
+nicksWords.removeValue(forKey: "test1") // usuwanie
